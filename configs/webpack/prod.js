9f4edf82f74786e63fea038450275b3da11f9152
Daniel Andrei <daniel.andrei@ezugi.com>
2020-01-31 15:21:50 +0200

    update

--- configs/webpack/prod.js
+++ configs/webpack/prod.js
@@ -10,7 +10,7 @@ module.exports = merge(commonConfig, {
   devtool: 'source-map',
   output: {
     filename: 'js/bundle.[hash].min.js',
-    path: resolve(__dirname, '../../dist'),
+    path: resolve(__dirname, '../../public'),
     publicPath: '/',
   },
   plugins: [],
