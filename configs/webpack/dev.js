e55f4ca7f3205ed8adab0eb0a01274204538e9e2
Daniel Andrei <daniel.andrei@ezugi.com>
2021-09-11 17:23:46 +0300

    chore: update dependencies
    remove react-spring

--- configs/webpack/dev.js
+++ configs/webpack/dev.js
@@ -1,27 +1,23 @@
 // development config
 const merge = require('webpack-merge').merge;
-const webpack = require('webpack');
 const commonConfig = require('./common');
 
 module.exports = merge(commonConfig, {
   mode: 'development',
   entry: [
     'react-hot-loader/patch', // activate HMR for React
-    'webpack-dev-server/client?http://localhost:8080',// bundle the client for webpack-dev-server and connect to the provided endpoint
+    'webpack-dev-server/client?http://localhost:8080', // bundle the client for webpack-dev-server and connect to the provided endpoint
     'webpack/hot/only-dev-server', // bundle the client for hot reloading, only- means to only hot reload for successful updates
-    './index.jsx' // the entry point of our app
+    './index.jsx', // the entry point of our app
   ],
   resolve: {
     alias: {
-      'react-dom': '@hot-loader/react-dom'
-    }
+      'react-dom': '@hot-loader/react-dom',
+    },
   },
   devServer: {
     historyApiFallback: true,
-    hot: true // enable HMR on the server
+    hot: true, // enable HMR on the server
   },
   devtool: 'cheap-module-source-map',
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(), // enable HMR globally
-  ]
 });
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { animated, useTrail } from 'react-spring';
 import Card from '../../components/Card';
 
 import { TEXT, IMAGE, ITEMS } from './constants';
 import './style.scss';
 
-function itemRenderer({ type, ...item }) {
+function itemRenderer({ type, ...item }, key) {
   switch (type) {
     case TEXT: {
-      return <span>{item.text}</span>;
+      return <span key={key}>{item.text}</span>;
     }
     case IMAGE: {
-      return <Card {...item} className="image" />;
+      return <Card key={key} {...item} className="image" />;
     }
     default: {
       return null;
@@ -20,33 +19,9 @@ function itemRenderer({ type, ...item }) {
 }
 
 export default function Home() {
-  const trail = useTrail(ITEMS.length, {
-    from: {
-      opacity: 0,
-      x: -200,
-    },
-    to: {
-      opacity: 1,
-      x: 0,
-    },
-  });
-
   return (
     <div className="home-wrapper">
-      {trail.map((props, key) => {
-        return (
-          <animated.div
-            style={{
-              ...props,
-              transform: props.x.interpolate(v => `translateX(${v}px)`),
-            }}
-            className="item"
-            key={key}
-          >
-            {itemRenderer(ITEMS[key])}
-          </animated.div>
-        );
-      })}
+      {ITEMS.map((item, index) => itemRenderer(item, index))}
     </div>
   );
 }
