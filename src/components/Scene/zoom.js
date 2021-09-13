5df1cf7f092a9815b84345882050c12174559910
Daniel Andrei <daniel.andrei@ezugi.com>
2021-09-13 12:42:40 +0300

    feat: increase zoom velocity

--- src/components/Scene/zoom.js
+++ src/components/Scene/zoom.js
@@ -2,5 +2,5 @@ import { useFrame } from '@react-three/fiber';
 
 export default function ZoomIn() {
   const vec = { x: -20, y: 30, z: -70 };
-  return useFrame(({ camera }) => camera.position.lerp(vec, 0.02));
+  return useFrame(({ camera }) => camera.position.lerp(vec, 0.1));
 }
